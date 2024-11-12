0cfbd4bd0dd902432acfc95fee0d48e38680e1fb
Divyanshu-t <tdivyanshu38@gmail.com>
2024-11-12 23:07:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// For smooth scrolling to different sections
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
